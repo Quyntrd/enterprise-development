a39e9efe6fff24d84b862642bc5abe4a9e730226
Quyntrd <daniil.terebilkin@gmail.com>
2025-11-17 15:43:56 +0400

    fixes

--- BicycleRental/BicycleRental.Domain/IRepository.cs
+++ BicycleRental/BicycleRental.Domain/IRepository.cs
@@ -14,30 +14,30 @@ public interface IRepository<TEntity, TKey>
     /// </summary>
     /// <param name="entity">Entity to create.</param>
     /// <returns>The created entity (may include assigned key).</returns>
-    TEntity Create(TEntity entity);
+    public TEntity Create(TEntity entity);
 
     /// <summary>
     /// Update an existing entity in the repository.
     /// </summary>
     /// <param name="entity">Entity with updated values (must contain identifier).</param>
-    void Update(TEntity entity);
+    public void Update(TEntity entity);
 
     /// <summary>
     /// Delete an entity by identifier.
     /// </summary>
     /// <param name="id">Identifier of the entity to delete.</param>
-    void Delete(TKey id);
+    public void Delete(TKey id);
 
     /// <summary>
     /// Read an entity by identifier.
     /// </summary>
     /// <param name="id">Identifier of the entity to read.</param>
     /// <returns>The entity if found; otherwise <c>null</c>.</returns>
-    TEntity? Read(TKey id);
+    public TEntity? Read(TKey id);
 
     /// <summary>
     /// Read all entities from the repository.
     /// </summary>
     /// <returns>List of all entities.</returns>
-    List<TEntity> ReadAll();
+    public List<TEntity> ReadAll();
 }
